5cbd8293b76434602854ed8dc47f4f3cde9da96e
HongMuchan <muchang0824@gmail.com>
2020-09-27 12:41:00 +0900

    [Add]classで継承してsuperの使い方

--- src/class.ts
+++ src/class.ts
@@ -1,30 +1,5 @@
-//constructorの引数にアクセス修飾子を書いている
-
-//---------------
-//省略前
-//---------------
 class Person {
-  name: string;
-  private age: number;
-
-  constructor(initName: string, initAge: number) {
-    this.name = initName;
-    this.age = initAge;
-  }
-
-  incrementAge() {
-    return console.log((this.age += 1));
-  }
-  greeting(this: Person) {
-    return console.log(`Hello My name is ${this.name}:${this.age}`);
-  }
-}
-//---------------
-//省略後
-//---------------
-
-class Person {
-  constructor(public name: string, private age: number) {}
+  constructor(public name: string, protected age: number) {} //継承先でも使えるprotected
 
   incrementAge() {
     return console.log((this.age += 1));
@@ -36,3 +11,23 @@ class Person {
 
 const quill = new Person("Quill", 30);
 quill.greeting();
+
+//---継承---
+//もし継承先でconstructorの引数を足したい時はsuperを用いる(subject)
+//①constructerの引数に型宣言
+//②superに継承元の引数を記入
+
+class Teacher extends Person {
+  constructor(name: string, age: number, public subject: string) {
+    super(name, age);
+  }
+  greeting() {
+    return console.log(
+      `Hello My name is ${this.name}:${this.age}:teach${this.subject}`
+    );
+  }
+}
+
+//subjectがたされてる
+const teacher = new Teacher("qiill", 20, "Math");
+teacher.greeting();
